cc55b64e8685f70614fc6a2050e9fb26a14573aa
Maxim <makson202007@gmail.com>
2025-07-09 13:01:29 +0300

    added solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const BIG_VACATION_DISCOUNT_DAYS = 7;
+  const BIG_VACATION_DISCOUNT_VALUE = 50;
+
+  const SMALL_VACATION_DISCOUNT_DAYS = 3;
+  const SMALL_VACATION_DISCOUNT_VALUE = 20;
+
+  const price = days * 40;
+
+  if (days >= BIG_VACATION_DISCOUNT_DAYS) {
+    return price - BIG_VACATION_DISCOUNT_VALUE;
+  }
+
+  if (days >= SMALL_VACATION_DISCOUNT_DAYS) {
+    return price - SMALL_VACATION_DISCOUNT_VALUE;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
